b0f88f231c00aabfbf6af432a861f4b8f8387ab4
vaishakhsnair <vaishakhsnair2004@gmail.com>
2024-04-05 18:06:49 +0530

    fix

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+console.log(process.env.SUPABASE_BASE_URL)
+console.log(process.env.SUPABASE_SECRET)
 
-const SUPABASE_URL = process.env.SUPABASE_BASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_SECRET;
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_BASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_SECRET;
 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
