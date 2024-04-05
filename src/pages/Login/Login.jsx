f12de157529075b119adac71c7002d0c109cf1b9
Jyothish <117960824+Luf1o@users.noreply.github.com>
2024-04-06 00:13:36 +0530

    some ... alot of changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import { FcGoogle } from "react-icons/fc";
 import { supabase } from '../../components/supabaseClient'
 import './login.css'
 
-    
+
 
 export default function Login() {
     const [session, setSession] = useState(null)
@@ -11,9 +11,9 @@ export default function Login() {
     useEffect(() => {
       supabase.auth.getSession().then(({ data: { session } }) => {
         setSession(session)
+        console.log('Login Page')
         console.log(session)
       })
-      console.log(session)
       const {
         data: { subscription },
       } = supabase.auth.onAuthStateChange((_event, session) => {
---
+++ src/pages/User/UserProfile.jsx
@@ -0,0 +1,16 @@
+import { HiBell,HiBellAlert } from "react-icons/hi2";
+import '../../styles/styles.css'
+
+const UserProfile = () =>{
+    return(
+        <div className="w-screen h-screen flex flex-col items-center justify-start relative bg-purple-950/[.3]">
+            <div className="blue-circle">Circle</div>
+            <div className="flex justify-between p-4 mb-4">
+
+            </div>
+        </div>
+    )
+}
+
+export default UserProfile;
+ 
