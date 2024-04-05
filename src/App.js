83d36866fbd3b2f16a2cd7c97ffeaf0b52ddd5a5
vaishakhsnair <vaishakhsnair2004@gmail.com>
2024-04-05 17:59:57 +0530

    created base home page and login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { supabase } from './components/supabaseClient'
 import './App.css';
+
 import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+      console.log(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+
+
+
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={session ? <Home />: <Login />} />
     </Routes>
   </Router>
   );
