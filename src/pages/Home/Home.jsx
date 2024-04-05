2398db52c8b387a60a1ab776b270c9e17eed63d3
vaishakhsnair <vaishakhsnair2004@gmail.com>
2024-04-06 00:21:42 +0530

    some thing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 import { supabase } from '../../components/supabaseClient'
 import { useState, useEffect } from 'react'
 
+import '../../components/background.css'
+
+
 
 export default function Home() {
     const [session, setSession] = useState(null)
@@ -19,17 +22,41 @@ export default function Home() {
   
       return () => subscription.unsubscribe()
     }, [])
-  
+
+    
+
+    const bg_with_circles = (
+      <div className='min-w-screen min-h-screen z-2 '>
+        <div className='relative top-[-50vw] -left-[70vw] w-[140vw] h-[140vw] bg-[#EAFED6] rounded-full z-0'></div>
+        <div className='relative top-[-10vw] right-0 w-[100vw] h-[100vw] bg-[#FEE5FD] rounded-full z-0'></div>
+      </div>
+    )
+
+
+    const menuButton = (name,icon,redirectTo) => (
+      <div className='min-w-11 min-h-11 border-2 border-[#337C54]'>
+
+      </div>
+    )
+
+    
+
     return (
-        <div
-            className='flex flex-col items-center justify-center min-w-screen min-h-screen 
-                        bg-image bg-no-repeat bg-cover 
-                        bg-center border-2 text-center'>
-            <div className='flex items-center justify-center 
-                        min-w-[90vw] rounded-xl min-h-20 bg-[#E3F4E1] gap-10 text-center
-                        text-xl cursor-pointer  '>
-                Home
-            </div>
+        <div className='relative min-w-screen min-h-screen max-h-screen flex-col '>
+           <div className='absolute w-full h-screen border-2 border-green-400 '>
+              <div className='relative'>
+                <div className='bg-circle-dsg'></div>
+                <div className='bg-circle-dsg'></div>
+                <div className='bg-circle-dsg'></div>
+                <div className='bg-circle-dsg'></div>
+              </div>
+           </div>
+          <div className='relative w-full h-full'>
+            <img src='/kswmp.png' className='relative w-screen  z-10'></img>
+          </div>
+
+            
+
         </div>
     )
 }
