036b27640c7a6daecad15ddfbaf0f562ea1a81d4
Vaishakh S Nair <36193436+vaishakhsnair@users.noreply.github.com>
2024-04-06 08:32:55 +0530

    Revert "links not working"

--- src/pages/Posts/Publishpost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PublishPost = () => {
-  return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center text-black'>PublishPost</div>
-  )
-}
-
-export default PublishPost
